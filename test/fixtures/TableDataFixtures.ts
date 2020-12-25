862d1567968ea00e0014d6c93402cb32946d317e
Igor Savin <iselwin@gmail.com>
2020-12-25 18:39:00 +0200

    Add sorting functionality

---
+++ test/fixtures/TableDataFixtures.ts
@@ -0,0 +1,13 @@
+import { TableData } from '../../lib/resultsExporter'
+
+export const VALID_TABLE_DATA: TableData = {
+  runtimeVersion: '10.23.0, V8 6.8.275.32-node.59',
+  benchmarkName: 'Dummy benchmark',
+  benchmarkEntryName: 'Dummy entry',
+  benchmarkEntryVersion: '1.0.0',
+  meanTimeNs: 101744870,
+  warmupCycles: 10,
+  label: 'dummy',
+  benchmarkCycleSamples: 50,
+  benchmarkCycles: 100,
+}
